9870de10af45a807eff8ab7a7cc4ec1b87f0cc57
Sean Griffin <griffin200@hotmail.com>
2018-12-17 15:25:20 +0000

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFstatements
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            string Start = "";
+            double num01;
+            double num02;
+
+            Console.Write("Enter a number to be devided: ");
+            num01 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a number to divide by: ");
+            num02 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num01 + " divide by " + num02 + " is equal to " + num01 / num02);
+
+            Console.ReadKey();
+            Console.WriteLine(); //Gonna read up on how to loop the application, but this is what i have so far. 
+          
+           
+        }
+    }
+}
