f6c2630e45614f414f0e922da1b2502b2560e011
Majdie Gideir <majdie40@hotmail.com>
2018-12-18 10:28:11 +0000

    Next task and notes.

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,14 +10,18 @@ namespace IFstatements
     {
         static void Main(string[] args)
         {
+            // maj note - next task is to filter out 0's and strings
             string Start = "";
             double num01;
             double num02;
 
             Console.Write("Put a number ere lad! and I'll divide it: ");
             num01 = Convert.ToDouble(Console.ReadLine());
+
             Console.Write("put a number ere to be divided by: ");
             num02 = Convert.ToDouble(Console.ReadLine());
+
+            // maj note - validate your values here
 
             Console.WriteLine(num01 + " divide by " + num02 + " is equal to " + num01 / num02);
 
