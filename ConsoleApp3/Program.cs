2a42ca74519b8908da82e59520a67d66c2fcac64
Majdie Gideir <majdie40@hotmail.com>
2018-12-20 09:52:48 +0000

    Hint for sean

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,7 +11,6 @@ namespace IFstatements
         static void Main(string[] args)
         {
             // maj note - next task is to filter out 0's and strings
-            string Start = "";
             double num01;
             double num02;
 
@@ -21,25 +20,22 @@ namespace IFstatements
             Console.Write("put a number ere to be divided by: ");
             num02 = Convert.ToDouble(Console.ReadLine());
 
-            string start = "0";
-
-            if (Start = 0) 
+            // you want to compare num01 and num02 with 0, if either is 0 than throw error
+            if (num01 == 0) // '==' to compare values, || used for or'ing 2 conditions 
             {
                 Console.WriteLine("error");
+                Console.WriteLine("Can't be devided");
             }
             else
             {
-                Console.WriteLine("Can't be devided");
-             
-            
-
                 Console.WriteLine(num01 + " divide by " + num02 + " is equal to " + num01 / num02);
-            
+
+
+                Console.ReadKey();
+                Console.WriteLine(); //Gonna read up on how to loop the application, but this is what i have so far. 
+            }
 
             Console.ReadKey();
-            Console.WriteLine(); //Gonna read up on how to loop the application, but this is what i have so far. 
-          
-           
         }
     }
 }
